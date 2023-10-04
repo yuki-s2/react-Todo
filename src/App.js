e4538830a08eddb1320e23fc9f9c36de5ad5bc2a
Yuki Honda <dahlia.15s2@gmail.com>
2023-10-04 23:43:54 +0900

    タスク追加機能

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import TodoList from './TodoList';
 
 function App() {
   const [todos, setTodos] = useState([{ id: 1, name: "Todo1", completed: false },]);
 
-  const handleAddTodo = () =>{};
+  const todoNameRef = useRef();
+  const handleAddTodo = () =>{
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: "1", name: name, completed: false }];
+    })
+    todoNameRef.current.value = null;
+  };
 
   return (
     <div>
       <TodoList todos={todos} />
-      <input type="text" />
+      <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク:0</div>
