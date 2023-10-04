57da1a1ee8bf8827f6984b83d6656b97ba5eeb96
Yuki Honda <dahlia.15s2@gmail.com>
2023-10-05 08:34:15 +0900

    uuid追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState([{ id: 1, name: "Todo1", completed: false },]);
@@ -8,7 +9,7 @@ function App() {
   const handleAddTodo = () =>{
     const name = todoNameRef.current.value;
     setTodos((prevTodos) => {
-      return [...prevTodos, { id: "1", name: name, completed: false }];
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
     })
     todoNameRef.current.value = null;
   };
