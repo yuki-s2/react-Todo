29d9015bcf1f069253f004a1bd1e70e3b67233f8
Yuki Honda <dahlia.15s2@gmail.com>
2023-10-06 22:38:08 +0900

    チェックボックス

--- src/App.js
+++ src/App.js
@@ -14,9 +14,16 @@ function App() {
     todoNameRef.current.value = null;
   };
 
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
   return (
     <div>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
